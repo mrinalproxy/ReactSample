18b9152f812caaa0ee2eede769f7002906c820e4
mrinalproxy <mrinalghosh360@gmail.com>
2022-11-08 20:44:39 +0530

    firstlessons

--- src/SecondSample.js
+++ src/SecondSample.js
@@ -3,25 +3,38 @@ import axios from "axios"
 
 
 const SecondComponent = () => {
-    function fetchData(){
+    function fetchData() {
         axios.get("https://hai98th.herokuapp.com/get_course/").then((res) => {
             setData(res.data)
+            console.log(res.data)
         }).catch(err => {
 
         })
     }
 
     // this line means fetchData always run 1 time
-    useEffect( fetchData, [] ) 
+    useEffect(fetchData, [])
 
-    const [data, setData] = useState([1,2,3])
+    const [data, setData] = useState([1, 2, 3])
 
-    return <div style={{marginTop: "100px", textAlign: "center"}}>
-        <div style={{border: "1px solid black"}}>
-            <p>Title: {data[0].title}</p>
-            <p>Content: {data[0].content}</p>
-        </div>
-        Hello world
+    return <div style={{ marginTop: "100px", textAlign: "center" }}>
+        {
+            data.map( (datum, id) =>{
+                return <div style={{
+                    border: "1px solid black",
+                    margin: "10px"
+                    
+                }}
+                key={id}
+                >
+
+                    <p>Title: {datum.title}</p>
+                    <p>Content: {datum.content}</p>
+                    <p>Id:{datum.id}</p>
+                    <p>Price:{datum.price}</p>
+                </div>
+            })
+        }
     </div>
 }
 
